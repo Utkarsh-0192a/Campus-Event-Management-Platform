bf70c6127c16a000d04a2c2bf194d9aac39579a1
Utkarsh-0192a <iutkarshgautam@gmail.com>
2025-01-25 00:38:27 +0530

    10

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const eventRoutes = require('./routes/events');
 const roleRoutes = require('./routes/roles');  // Import role routes
-
+const path = require('path');
 // Initialize dotenv for environment variables
 dotenv.config();
 
 // Initialize Express app
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 // Middleware
 app.use(bodyParser.json());
@@ -26,6 +29,32 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/roles', roleRoutes);  // Use role routes
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'template', 'index.html'));
+});
+
+// PAGES CONNECTING TO HOME
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'template', 'login.html'));
+});
+
+app.post('/submit/login', (req, res) => {
+  const { username, password, role } = req.body;
+
+  const result = `The concatenated string is: ${username}${password}${role}`;
+
+  res.json({ message: result });
+});
+
+app.post('/submit/register', (req, res) => {
+  const { name, email, username, password, role } = req.body;
+
+  const result = `The concatenated string is: ${name}, ${email}, ${username}, ${password} ${role}`;
+
+  res.json({ message: result });
+});
+
 
 // Start the server
 const PORT = process.env.PORT || 5000;
