30e56d5dc0247e798a0d3da83e78f60f5d7dfc36
Dipanshu Tiwari <dipanshutiwari115@gmail.com>
2025-01-24 21:33:37 +0530

    01

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'template', 'index.html'));
 });
 
-const PORT = 3000;
+// PAGES CONNECTING TO HOME
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'template', 'login.html'));
+});
+
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
